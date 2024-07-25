641f3bde844436e01b822c591fc43009a5d8c5e1
Lucassamuel97 <lukassamuka88@gmail.com>
2024-07-24 21:58:19 -0300

    entity customer

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,31 +1,43 @@
 class Custumer{
   _id: string;
   _name: string;
-  _address: string;
+  _address: string = "";
+  _active: boolean = true;
 
-  constructor(id: string, name: string, address: string){
+  constructor(id: string, name: string){
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validate();
   }
 
-  get id(): string{
-    return this._id;
-  }
-  get name(): string{
-    return this._name;
-  }
-  get address(): string{
-    return this._address;
+  validate(){
+    if(this._name === ""){
+      throw new Error("Name is required");
+    }
+
+    if(this._id === ""){
+      throw new Error("Id is required");
+    }
   }
 
-  set id(id: string){
-    this._id = id;
-  }
   set name(name: string){
     this._name = name;
   }
-  set address(address: string){
-    this._address = address;
+
+  changeName(name: string){
+    this._name = name;
+    this.validate();
   }
+
+  activate(){
+    if(this._address.length === 0){
+      throw new Error("Address is mandatory to activate a customer");
+    }
+    this._active = true;
+  }
+  
+  deactivate(){
+    this._active = false;
+  }
+
 }
