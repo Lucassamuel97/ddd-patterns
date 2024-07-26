c78548d9ff44fbc427d8706b40b2fd854a782a24
Lucassamuel97 <lukassamuka88@gmail.com>
2024-07-26 09:17:23 -0300

    created product entity, adjusted other classes to calculate by product

--- src/entity/order.ts
+++ src/entity/order.ts
@@ -1,13 +1,13 @@
 import OrderItem from "./order_item";
 
-export default class Order{
+export default class Order {
 
     private _id: string;
     private _customerId: string;
     private _items: OrderItem[];
     private _total: number = 0;
 
-    constructor(id: string, customerId: string, items: OrderItem[]){
+    constructor(id: string, customerId: string, items: OrderItem[]) {
         this._id = id;
         this._customerId = customerId;
         this._items = items;
@@ -15,22 +15,37 @@ export default class Order{
         this.validate();
     }
 
-    validate(): boolean{
-        if(this._id === ""){
+    get id(): string {
+        return this._id;
+    }
+
+    get customerId(): string {
+        return this._customerId;
+    }
+
+    get items(): OrderItem[] {
+        return this._items;
+    }
+
+    validate(): boolean {
+        if (this._id === "") {
             throw new Error("Id is required");
         }
-        if(this._customerId === ""){
+        if (this._customerId === "") {
             throw new Error("Customer Id is required");
         }
         if (this._items.length === 0) {
             throw new Error("Items are required");
         }
+        if (this._items.some((item) => item.quantity <= 0)) {
+            throw new Error("Quantity must be greater than 0");
+        }
 
         return true;
     }
 
-    total(): number{
-        return this._items.reduce((acc, item) => acc + item.price, 0);
+    total(): number {
+        return this._items.reduce((acc, item) => acc + item.total(), 0);
     }
 
 }
