b9f53ae824e030a91ffa1e1c8f4d64682b0bc79e
Lucassamuel97 <lukassamuka88@gmail.com>
2024-07-26 16:06:24 -0300

    OrderService created, rewards program implemented

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -5,14 +5,14 @@ describe("Customer unit tests", () => {
 
     it("should trow error when id is empty", () => {
 
-        expect(() => { 
+        expect(() => {
             let customer = new Customer("", "John Doe");
         }).toThrowError("Id is required");
 
     });
 
     it("should trow error when name is empty", () => {
-        expect(() => { 
+        expect(() => {
             let customer = new Customer("123", "");
         }).toThrowError("Name is required");
     });
@@ -25,7 +25,7 @@ describe("Customer unit tests", () => {
 
     it("should activate customer", () => {
         const customer = new Customer("1", "Custumer 1");
-        const address = new Address("Main Street", 123, "12345678","Palmitla");
+        const address = new Address("Main Street", 123, "12345678", "Palmitla");
         customer.Address = address;
         customer.activate();
         expect(customer.isActive()).toBe(true);
@@ -33,18 +33,28 @@ describe("Customer unit tests", () => {
 
     it("should throw error when address is undefined when you activate a customer", () => {
         expect(() => {
-          const customer = new Customer("1", "Customer 1");
-          customer.activate();
+            const customer = new Customer("1", "Customer 1");
+            customer.activate();
         }).toThrowError("Address is mandatory to activate a customer");
     });
 
     it("should deactivate customer", () => {
         const customer = new Customer("1", "Customer 1");
-    
+
         customer.deactivate();
-    
+
         expect(customer.isActive()).toBe(false);
     });
-    
+
+    it("should add reward points", () => {
+        const customer = new Customer("1", "Customer 1");
+        expect(customer.rewardPoints).toBe(0);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(10);
+
+        customer.addRewardPoints(10);
+        expect(customer.rewardPoints).toBe(20);
+    });
 
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,6 +5,7 @@ export default class Custumer{
   private _name: string;
   private _address!: Address;
   private _active: boolean = true;
+  private _rewardPoints: number = 0;
 
   constructor(id: string, name: string){
     this._id = id;
@@ -12,8 +13,16 @@ export default class Custumer{
     this.validate();
   }
 
+  get id(): string{
+    return this._id;
+  }
+
   get name(): string{
     return this._name;
+  }
+
+  get rewardPoints(): number{
+    return this._rewardPoints;
   }
 
   isActive(): boolean{
@@ -60,4 +69,9 @@ export default class Custumer{
   set Address(address: Address) {
     this._address = address;
   }
+
+  addRewardPoints(points: number){
+    this._rewardPoints += points;
+  }
+
 }
